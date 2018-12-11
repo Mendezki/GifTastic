52fa5bf48bb23b02761ee3d34cef2a102010fba0
Mendezki <menluca@gmail.com>
2018-12-10 21:12:31 -0500

    message

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,71 @@
 //$(document).ready(function () {
+// var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=Rolling Stones&api_key=S2twFv9aoRxe4X8az44kMKL14WzVHQ0t&limit=10");
+// xhr.done(function (data) { console.log("success got data", data); });
 
-var bands = ["Beatles","Rolling Stones","The Beach Boys","Led Zeppelin","Bo Diddley","Pink Floyd","The Clash","The Kinks","Velvet Underground","Dead Kennedys"];
+
+var bands = ["Beatles", "Rolling Stones", "The Beach Boys", "Led Zeppelin", "Bo Diddley", "Pink Floyd", "The Clash", "The Kinks", "Velvet Underground", "Dead Kennedys"];
 
 $("#add-band").on("click", function () {
-    
+
     var band = $("#band-input").val().trim()
     bands.push(band);
     renderButtons();
     return false;
 });
 
+// $(document).on("click", ".band", function () {
+//     var band = $(this).attr("data-name");
+//     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + band + "&api_key=S2twFv9aoRxe4X8az44kMKL14WzVHQ0t&limit=10";
+
+
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).done(function (response) {
+//         console.log(queryURL);
+
+
+//         var results = response.data;
+
+//         for (var i = 0; i < results.length; i++) {
+
+//             var bandDiv = $("<div>");
+//             bandDiv.addClass("bandGifs");
+
+//             var bandGif = $("<img>");
+//             bandGif.attr("src", results[i].images.original_still.url);
+//             bandGif.attr("data-still", results[i].images.original_still.url);
+//             bandGif.attr("data-animate", results[i].images.original.url);
+//             bandGif.attr("data-state", "still");
+//             bandGif.attr("class", "gif");
+
+//             bandDiv.append(bandGif)
+
+//             $("#gifs-view").prepend(bandDiv);
+
+//             console.log(results);
+//         }
+
+//     });
+    
+// });
+
 function displayGifs() {
     var band = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + band + "&api_key=S2twFv9aoRxe4X8az44kMKL14WzVHQ0t&limit=10";
-    
-    
+
+
     $.ajax({
         url: queryURL,
         method: "GET"
     }).done(function (response) {
         console.log(queryURL);
-        
-        
+
+
         var results = response.data;
-        
+
         for (var i = 0; i < results.length; i++) {
-            
+
             var bandDiv = $("<div>");
             bandDiv.addClass("bandGifs");
 
@@ -35,11 +75,11 @@ function displayGifs() {
             bandGif.attr("data-animate", results[i].images.original.url);
             bandGif.attr("data-state", "still");
             bandGif.attr("class", "gif");
-            
+
             bandDiv.append(bandGif)
 
             $("#gifs-view").prepend(bandDiv);
-            
+
             console.log(results);
         }
 
@@ -59,18 +99,18 @@ function renderButtons() {
     }
 }
 
-   $(".gif").on("click", function() {
-      var state = $(this).attr("data-state");
-      if (state === "still") {
+$(document).on("click", ".gif", function () {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
-      } else {
+    } else {
         $(this).attr("src", $(this).attr("data-still"));
         $(this).attr("data-state", "still");
-        }
-    });
+    }
+});
 
-$(document).on("click", ".gif", displayGifs);
+$(document).on("click", ".band", displayGifs);
 
 renderButtons();
 //});
